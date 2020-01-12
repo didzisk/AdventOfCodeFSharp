44087f366ee436e6824beebac9ba4c34b9929d22
Didzis Klavins <didzis.klavins@visma.com>
2020-01-12 17:40:05 +0100

    Day 3 Wire generation by unfolding

--- 2019Day3/Wires.fs
+++ 2019Day3/Wires.fs
@@ -22,30 +22,25 @@ let wire (s:string) :point list =
         s.Split [|','|]
         |> Array.map (fun x-> x.Trim [|' '|])
 
-    seq{
-        let folder _ elm =
+    let folder (currentPoint,currentList) elm =
 
-            let unfolder (point, command:string) = 
-                let dir=command.[0..0]
-                let num = int command.[1..] - 1
-                if num=(-1) then
-                    None
-                else
-                    let nextCommand=dir+string num
-                    let nextPoint=
-                        match dir with 
-                        | "R" -> ({point with X=point.X+1; dist = point.dist+1})
-                        | "L" -> ({point with X=point.X-1; dist = point.dist+1})
-                        | "U" -> ({point with Y=point.Y-1; dist = point.dist+1})
-                        | _   -> ({point with Y=point.Y+1; dist = point.dist+1})
-                    Some (nextPoint,(nextPoint, nextCommand))
-            let kk= Seq.unfold unfolder ({X=0;Y=0;dist=0}, elm)
-            
-            yield! kk
-
-            elm
-        arr |> Array.fold folder
-    }
-    |> Seq.toList
-    
+        let unfolder (point, command:string) = 
+            let dir=command.[0..0]
+            let num = int command.[1..] - 1
+            if num=(-1) then
+                None
+            else
+                let nextCommand=dir+string num
+                let nextPoint=
+                    match dir with 
+                    | "R" -> ({point with X=point.X+1; dist = point.dist+1})
+                    | "L" -> ({point with X=point.X-1; dist = point.dist+1})
+                    | "U" -> ({point with Y=point.Y-1; dist = point.dist+1})
+                    | _   -> ({point with Y=point.Y+1; dist = point.dist+1})
+                Some (nextPoint,(nextPoint, nextCommand))
+        let kk= List.unfold unfolder (currentPoint, elm)
+        
+        (Seq.last kk, currentList @ kk)
+    let (st,gg)=arr |> Array.fold folder ({X=0; Y=0; dist=0},[])
+    gg
     
