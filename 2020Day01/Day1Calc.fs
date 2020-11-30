fc1dc3b8adfed0d1c14752c04cbba94fd9874a67
Didzis Klavins <didzis.klavins@visma.com>
2020-11-30 21:30:57 +0100

    2020 day1 template

---
+++ 2020Day01/Day1Calc.fs
@@ -0,0 +1,10 @@
+﻿module Day1Calc
+
+open System.IO
+
+let Lines filename = 
+    File.ReadLines filename
+    |> Seq.map int32
+
+
+
---
+++ 2020Day01/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day1Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let a=Lines "Day1Input.txt"
+    a
+    |> Seq.iter (fun x->
+        printfn "%A" x
+    )
+
+
+    0 // return an integer exit code
