c08107b886b8b306951b043f8bf95485e1279089
Didzis Klavins <didzis.klavins@visma.com>
2020-12-03 08:45:15 +0100

    Day 3 in 2020 completed

--- 2020Day03/Day3Calc.fs
+++ 2020Day03/Day3Calc.fs
@@ -10,3 +10,44 @@ let aLines filename =
     |> Seq.map int32
     |> Seq.toArray
 
+let parseInput (lines:string seq) =
+    lines |> Seq.toArray
+
+(*    seq {
+        for x=0 to (arr.[0].Trim()).Length-1 do
+            for y=0 to arr.Length-1 do
+                if arr.[y].[x] = '#' then
+                    yield (x,y)
+    }
+    *)
+
+let parseSize (lines:string seq) =
+    let arr=lines |> Seq.toArray
+    arr.Length (*rows*), arr.[0].Trim().Length (*cols*)
+
+let rec downOneStep (grid:string[]) (rowsD:int) (colsR:int) currPos =
+    let row, col, currCount = currPos
+    let rows, cols = parseSize grid
+    if row+rowsD>=rows then
+        printfn "Result %A" currCount
+        currCount
+    else
+        let nextRow, nextCol = 
+            if (col+colsR >= cols) then
+                row+rowsD, col+colsR-cols
+            else
+                row+rowsD, col+colsR
+
+        if grid.[nextRow].[nextCol]='#' then
+//            printfn "%A row:%A col:%A" currCount nextRow nextCol 
+            downOneStep grid rowsD colsR (nextRow, nextCol, currCount+1)
+        else
+//            printfn "%A row:%A col:%A" currCount nextRow nextCol 
+            downOneStep grid rowsD colsR (nextRow, nextCol, currCount)
+
+let solve rows cols filename =
+    let pInput =
+        filename
+        |> Lines
+        |> parseInput
+    downOneStep pInput rows cols (0, 0, 0)
--- 2020Day03/Program.fs
+++ 2020Day03/Program.fs
@@ -4,5 +4,29 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Right 3 down 1"
+    Day3Calc.solve 1 3 "Day3InputExample.txt"
+    |> ignore
+    Day3Calc.solve 1 3 "Day3Input.txt"
+    |> ignore
+
+    Day3Calc.solve 1 1 "Day3InputExample.txt"
+    |> ignore
+    Day3Calc.solve 1 3 "Day3InputExample.txt"
+    |> ignore
+    Day3Calc.solve 1 5 "Day3InputExample.txt"
+    |> ignore
+
+    printfn ""
+
+    Day3Calc.solve 1 1 "Day3Input.txt"
+    |> ignore
+    Day3Calc.solve 1 3 "Day3Input.txt"
+    |> ignore
+    Day3Calc.solve 1 5 "Day3Input.txt"
+    |> ignore
+    Day3Calc.solve 1 7 "Day3Input.txt"
+    |> ignore
+    Day3Calc.solve 2 1 "Day3Input.txt"
+    |> ignore
     0 // return an integer exit code
