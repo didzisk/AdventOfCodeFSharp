4a830143b2b435dd9238ba27e10e6d3285419d30
Didzis Klavins <didzis.klavins@visma.com>
2020-12-03 11:02:29 +0100

    Day 3 in 2020 improved

--- 2020Day03/Day3Calc.fs
+++ 2020Day03/Day3Calc.fs
@@ -5,21 +5,8 @@ open System.IO
 let Lines filename = 
     File.ReadLines filename
 
-let aLines filename = 
-    File.ReadLines filename
-    |> Seq.map int32
-    |> Seq.toArray
-
 let parseInput (lines:string seq) =
     lines |> Seq.toArray
-
-(*    seq {
-        for x=0 to (arr.[0].Trim()).Length-1 do
-            for y=0 to arr.Length-1 do
-                if arr.[y].[x] = '#' then
-                    yield (x,y)
-    }
-    *)
 
 let parseSize (lines:string seq) =
     let arr=lines |> Seq.toArray
@@ -29,7 +16,6 @@ let rec downOneStep (grid:string[]) (rowsD:int) (colsR:int) currPos =
     let row, col, currCount = currPos
     let rows, cols = parseSize grid
     if row+rowsD>=rows then
-        printfn "Result %A" currCount
         currCount
     else
         let nextRow, nextCol = 
@@ -39,15 +25,14 @@ let rec downOneStep (grid:string[]) (rowsD:int) (colsR:int) currPos =
                 row+rowsD, col+colsR
 
         if grid.[nextRow].[nextCol]='#' then
-//            printfn "%A row:%A col:%A" currCount nextRow nextCol 
             downOneStep grid rowsD colsR (nextRow, nextCol, currCount+1)
         else
-//            printfn "%A row:%A col:%A" currCount nextRow nextCol 
             downOneStep grid rowsD colsR (nextRow, nextCol, currCount)
 
-let solve rows cols filename =
+let solve rows cols filename : int64 =
     let pInput =
         filename
-        |> Lines
-        |> parseInput
+        |> File.ReadLines
+        |> Seq.toArray
     downOneStep pInput rows cols (0, 0, 0)
+    |> int64
--- 2020Day03/Program.fs
+++ 2020Day03/Program.fs
@@ -3,30 +3,28 @@
 open System
 
 [<EntryPoint>]
-let main argv =
-    printfn "Right 3 down 1"
+let main _ =
+
     Day3Calc.solve 1 3 "Day3InputExample.txt"
-    |> ignore
+    |> printfn "Pt1 Example D1 R3: %d"
+
     Day3Calc.solve 1 3 "Day3Input.txt"
-    |> ignore
+    |> printfn "Pt1 Solution D1 R3: %d"
 
     Day3Calc.solve 1 1 "Day3InputExample.txt"
-    |> ignore
-    Day3Calc.solve 1 3 "Day3InputExample.txt"
-    |> ignore
-    Day3Calc.solve 1 5 "Day3InputExample.txt"
-    |> ignore
+    * Day3Calc.solve 1 3 "Day3InputExample.txt"
+    * Day3Calc.solve 1 5 "Day3InputExample.txt"
+    * Day3Calc.solve 1 7 "Day3InputExample.txt"
+    * Day3Calc.solve 2 1 "Day3InputExample.txt"
+    |> printfn "Pt2 Example: %d"
 
     printfn ""
 
     Day3Calc.solve 1 1 "Day3Input.txt"
-    |> ignore
-    Day3Calc.solve 1 3 "Day3Input.txt"
-    |> ignore
-    Day3Calc.solve 1 5 "Day3Input.txt"
-    |> ignore
-    Day3Calc.solve 1 7 "Day3Input.txt"
-    |> ignore
-    Day3Calc.solve 2 1 "Day3Input.txt"
-    |> ignore
+    * Day3Calc.solve 1 3 "Day3Input.txt"
+    * Day3Calc.solve 1 5 "Day3Input.txt"
+    * Day3Calc.solve 1 7 "Day3Input.txt"
+    * Day3Calc.solve 2 1 "Day3Input.txt"
+    |> printfn "Pt2 Solution: %d"
+
     0 // return an integer exit code
