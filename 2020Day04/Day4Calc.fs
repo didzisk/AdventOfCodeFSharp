f6e291fe73fe1bf6732d15e6d101f640cc6e8d5a
Didzis Klavins <didzis.klavins@visma.com>
2020-12-04 21:50:15 +0100

    Day 4 in 2020 completed

--- 2020Day04/Day4Calc.fs
+++ 2020Day04/Day4Calc.fs
@@ -1,71 +1,147 @@
 ﻿module Day4Calc
 
 open System.IO
+open Common
 
 let Lines filename = 
     File.ReadLines filename
-
-let aLines filename = 
-    File.ReadLines filename
-    |> Seq.map int32
     |> Seq.toArray
 
-let numFrom (line:string) =
-    let a=line.IndexOf('-')
-    let c=line.Substring(0,a)
-    int32 c
+let passports lines =
+    let mutable s = ""
+    seq {
+        for l in lines do
+            if l = "" then
+                yield s
+                s <- ""
+            else
+                s <- s+" "+l
+        yield s
+        }
 
-let numTo (line:string)=
-    let a=line.IndexOf('-')+1
-    let b=line.IndexOf(' ')-a
-    let c=line.Substring(a,b)
-    int32 c
+let createPassport (line:string) =
+    let fields=split " " line
+    fields
+    |> Array.map (fun x -> 
+        let elem=split ":" x
+        (elem.[0],elem.[1])
+        )
+    |> Map.ofArray
 
-let charMy (line:string)=
-    let b=line.IndexOf(' ')
-    let c=line.[b+1]
-    c
+let containsField (passport:Map<string,string>) s =
+    passport |> Map.containsKey s
 
-let numMy (line:string) (my:char) =
-    let a=
-        line.ToCharArray()
-        |> Array.filter (fun x-> x=my)
+let passportValid (passport:Map<string,string>) =
+    let reqFields = [|"byr"; "iyr"; "eyr"; "hgt"; "hcl"; "ecl"; "pid"|]
+    let numFields=
+        reqFields
+        |> Array.filter 
+            (containsField passport)
+            
         |> Array.length
-    a-1
+    numFields>6
 
-        
+//byr (Birth Year) - four digits; at least 1920 and at most 2002.
+let byrValid (passport:Map<string,string>) =
+    let s = passport.Item "byr"
 
-let pwdValid (line:string) =
-    let a=numFrom line
-    let b=numTo line
-    let my=charMy line
-    let c=numMy line my
-    (c>=a) && (c<=b) 
+    match System.Int32.TryParse s with
+    | true, a -> 
+        a>=1920 && a<=2003
+    | _ -> false
 
-let strippedLine (line:string) =
-    let a=line.IndexOf(':')
-    let res=line.Substring(a+2)
-    res
+//iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+let iyrValid (passport:Map<string,string>) =
+    let s = passport.Item "iyr"
 
-let pwdValid2 (line:string) =
-    let a=numFrom line
-    let b=numTo line
-    let my=charMy line
-    let s=strippedLine line
-    let firstchar=s.[a-1]
-    let secondchar=s.[b-1]
-    let firstgood=my=firstchar
-    let secondgood=my=secondchar
-    let allgood = firstgood <> secondgood
-    allgood
+    match System.Int32.TryParse s with
+    | true, a -> 
+        a>=2010 && a<=2020
+    | _ -> false
 
-let linesValid lines =
-    lines
-    |> Seq.filter pwdValid
-    |> Seq.length
+//eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+let eyrValid (passport:Map<string,string>) =
+    let s = passport.Item "eyr"
 
-let linesValid2 lines =
-    lines
-    |> Seq.filter pwdValid2
-    |> Seq.length
+    match System.Int32.TryParse s with
+    | true, a -> 
+        a>=2020 && a<=2030
+    | _ -> false
+
+//hgt (Height) - a number followed by either cm or in:
+//If cm, the number must be at least 150 and at most 193.
+//If in, the number must be at least 59 and at most 76.
+let hgtValid (passport:Map<string,string>) =
+    let s = passport.Item "hgt"
+
+    let inc=index "in" s
+    if inc > 0 then
+        let hin = s.Substring(0,inc)
+
+        match System.Int32.TryParse hin with
+        | true, a -> 
+            a>=59 && a<=76
+        | _ -> false
+    else 
+        let cmc=index "cm" s
+        if cmc>0 then
+            let hcm = s.Substring(0,cmc)
+            match System.Int32.TryParse hcm with
+            | true, a -> 
+                a>=150 && a<=193
+            | _ -> false
+        else
+            false
+//hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+let hclValid (passport:Map<string,string>)  =
+    let s = passport.Item "hcl"
+
+    let hashPos = index "#" s
+    if hashPos<>0 then
+        false
+    else
+        let hex=s.Substring(1)
+        if hex.Length <> 6 then
+            false
+        else
+            try
+                System.Int32.Parse(hex, System.Globalization.NumberStyles.HexNumber)
+                |> ignore
+                true
+            with
+            | _ -> false 
+
+
+//ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+let eclValid (passport:Map<string,string>)  =
+    let s = passport.Item "ecl"
+
+    let validCols = ["amb"; "blu"; "brn"; "gry"; "grn"; "hzl"; "oth"]
+    validCols
+    |> List.contains s
+
+//pid (Passport ID) - a nine-digit number, including leading zeroes.
+let pidValid (passport:Map<string,string>)  =
+    let s = passport.Item "pid"
+    if s.Length=9 then
+        try
+            System.Int64.Parse(s)
+            |> ignore
+            true
+        with
+        | _ -> false 
+    else
+        false
+
+
+
+let passportValid2 (passport:Map<string,string>) =
+    passportValid passport
+    && byrValid passport
+    && iyrValid passport
+    && eyrValid passport
+    && hgtValid passport
+    && hclValid passport
+    && eclValid passport
+    && pidValid passport
 
--- 2020Day04/Program.fs
+++ 2020Day04/Program.fs
@@ -1,8 +1,47 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Day4Calc
 
 [<EntryPoint>]
-let main argv =
+let main _ =
     printfn "Hello World from F#!"
+    Lines "Day4inputExample.txt"
+    |> passports
+    |> Seq.toArray
+    |> printfn "%A"
+
+    Lines "Day4inputExample.txt"
+    |> passports
+    |> Seq.map createPassport
+    |> printfn "%A"
+
+    Lines "Day4input.txt"
+    |> passports
+    |> Seq.map createPassport
+    |> Seq.filter passportValid
+    |> Seq.length
+    |> printfn "%A"
+
+    Lines "Day4inputExampleValids.txt"
+    |> passports
+    |> Seq.map createPassport
+    |> Seq.filter passportValid2
+    |> Seq.length
+    |> printfn "%A"
+
+    Lines "Day4inputExampleInvalids.txt"
+    |> passports
+    |> Seq.map createPassport
+    |> Seq.filter passportValid2
+    |> Seq.length
+    |> printfn "%A"
+
+    Lines "Day4input.txt"
+    |> passports
+    |> Seq.map createPassport
+    |> Seq.filter passportValid2
+    |> Seq.length
+    |> printfn "Part 2 result: %A"
+
     0 // return an integer exit code
