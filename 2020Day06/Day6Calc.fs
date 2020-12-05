a1dbf57f2bd20ce3f70c7358dbf60cf0c2851dfa
Didzis Klavins <didzis.klavins@visma.com>
2020-12-05 07:54:22 +0100

    Day 6 template

---
+++ 2020Day06/Day6Calc.fs
@@ -0,0 +1,40 @@
+﻿module Day6Calc
+
+open System.IO
+open Common
+
+let Lines filename = 
+    File.ReadLines filename
+    |> Seq.toArray
+
+let createSeat2 (line:string) =
+    line.ToCharArray()
+    |> Array.mapi (
+        fun i x -> 
+            let mask = 0b1000000000 >>> i
+            if (x='B' || x='R') then
+                mask
+            else
+                0
+        )
+    |> Array.sum
+
+let bitmaskToChar id mask =
+    if id &&& mask = mask then
+        if mask>7 then
+            'R'
+        else
+            'B'
+    else
+        if mask>7 then
+            'L'
+        else
+            'F'
+
+let idToSeat (id:int) =
+    seq {
+        for i= 0 to 9 do
+            let mask = 0b1000000000 >>> i
+            yield bitmaskToChar id mask
+            }
+    |> asString
---
+++ 2020Day06/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day6Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    for i = 89 to 1023 do
+        idToSeat i
+        |> printfn "%s"
+
+    Lines "Day6Input.txt"
+    |> Array.iter (printfn "%s")
+
+
+    0 // return an integer exit code
