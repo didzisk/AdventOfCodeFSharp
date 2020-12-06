b4523b01ab33f29c21b52217fed09563c5d3b94c
Didzis Klavins <didzis.klavins@visma.com>
2020-12-06 21:35:14 +0100

    Day 7 template

---
+++ 2020Day07/Day7Calc.fs
@@ -0,0 +1,76 @@
+﻿module Day7Calc
+
+open System.IO
+open Common
+
+let Lines filename = 
+    File.ReadLines filename
+    |> Seq.toArray
+
+let answers lines =
+    let mutable s = ""
+    seq {
+        for l in lines do
+            if l = "" then
+                yield s
+                s <- ""
+            else
+                s <- s+l
+        yield s
+        }
+
+let uniqueAnswers (a:string) =
+    a.ToCharArray()
+    |> Set.ofArray
+    |> Set.count
+
+let sumUniqueAnswers (inp:string seq) =
+    inp
+    |> Seq.map uniqueAnswers
+    |> Seq.sum
+
+let part1 filename =
+    Lines filename
+    |> answers
+    |> sumUniqueAnswers
+
+    
+let answerSets lines =
+    let mutable s = List.empty
+    seq {
+        for l in lines do
+            if l = "" then
+                yield s
+                s <- List.empty
+            else
+                let newSet=
+                    l.ToCharArray()
+                    |> Set.ofArray
+                s<-newSet::s
+                
+        yield s
+        }
+
+let innerEqualAnswers first (s:Set<char> list)  =
+    let action resultSoFar x = Set.intersect resultSoFar x
+    s |> List.fold action first
+    |> Set.count
+
+let equalAnswers (s:Set<char> list) =
+    match s with
+    | [] -> 
+        0
+    | first::rest -> 
+        let max = innerEqualAnswers first rest
+        max
+
+
+  
+let part2 filename =
+    Lines filename
+    |> answerSets
+    |> Seq.map equalAnswers
+    |> Seq.sum
+    |> printfn "%A"
+
+  
---
+++ 2020Day07/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day7Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    Lines "Day7InputExample.txt"
+    |> Array.iter (printfn "%A")
+
+    0 // return an integer exit code
