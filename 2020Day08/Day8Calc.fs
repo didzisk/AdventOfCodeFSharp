7cd3c3611cba9a74bdbbadda6d56260585938bca
Didzis Klavins <didzis.klavins@visma.com>
2020-12-08 19:04:26 +0100

    Day 8 in 2020 refactored out the machine

--- 2020Day08/Day8Calc.fs
+++ 2020Day08/Day8Calc.fs
@@ -2,63 +2,11 @@
 
 open System.IO
 open Common
-open System.Text.RegularExpressions
+open Handheld.Machine
 
 let Lines filename = 
     File.ReadLines filename
     |> Seq.toArray
-
-(*
-nop +0  | 1
-acc +1  | 2, 8(!)
-jmp +4  | 3
-acc +3  | 6
-jmp -3  | 7
-acc -99 |
-acc +1  | 4
-jmp -4  | 5
-acc +6  |
-
-*)
-
-
-type MachineState = 
-    {
-        PC:int;
-        Program:string[];
-        //Inputs:List<int64>;
-        VisitedLocations:Set<int>;
-        Acc:int
-        //RelativeBase:int;
-        //Result:int64
-        TerminatedNormally: Option<bool>
-    }
-
-let rec doOne (state:MachineState) : MachineState =
-    let line = split " " state.Program.[state.PC]
-    let cmd = line.[0]
-    let arg0 = int32 line.[1]
-    let pc =
-        match cmd with
-        | "jmp" -> state.PC + arg0
-        | _ -> state.PC + 1
-    let acc =
-        match cmd with
-        | "acc" -> state.Acc + arg0
-        | _ -> state.Acc
-    if pc<state.Program.Length then
-        if state.VisitedLocations.Contains pc then
-            {state with TerminatedNormally = Some false}
-        else
-            {state with 
-                PC = pc; 
-                VisitedLocations = state.VisitedLocations.Add pc; 
-                Program = state.Program;
-                Acc = acc
-            }
-            |> doOne  
-    else
-        {state with TerminatedNormally = Some true}
 
 let calc1 lines =
     let ms:MachineState =
@@ -67,9 +15,9 @@ let calc1 lines =
             VisitedLocations = Set.empty |> Set.add 0;
             Program = lines;
             Acc = 0;
-            TerminatedNormally = None
+            RunState = Continue
         }
-    let res = doOne ms
+    let res = Handheld.Machine.Run ms
     res.Acc
 
 let rec calc2 place (lines:string array) =
@@ -80,7 +28,7 @@ let rec calc2 place (lines:string array) =
         match cmd with
         | "jmp" -> "nop"
         | "nop" -> "jmp"
-        | _ -> "acc"
+        | a -> a
 
     let newline = newcmd + " " + arg0
 
@@ -95,11 +43,11 @@ let rec calc2 place (lines:string array) =
             VisitedLocations = Set.empty |> Set.add 0;
             Program = newLines;
             Acc = 0;
-            TerminatedNormally = None
+            RunState = Continue
         }
 
-    let res = doOne st
-    if res.TerminatedNormally = Some true then
+    let res = Handheld.Machine.Run st
+    if res.RunState = ReachedEnd then
         res.Acc
     else
         calc2 (place+1) lines 
---
+++ Handheld/Machine.fs
@@ -0,0 +1,62 @@
+﻿module Handheld.Machine
+
+open Common
+
+type TerminationReason=
+    | Continue
+    | ReachedEnd
+    | LoopedBack
+
+type MachineState = 
+    {
+        PC:int;
+        Program:string[];
+        //Inputs:List<int64>;
+        VisitedLocations:Set<int>;
+        Acc:int
+        //RelativeBase:int;
+        //Result:int64
+        RunState: TerminationReason
+    }
+
+let private doAcc (state:MachineState) arg : MachineState =
+    {state with
+        PC=state.PC+1;
+        Acc = state.Acc + arg;
+    }
+
+let private doJmp (state:MachineState) arg : MachineState =
+    {state with
+        PC=state.PC+arg;
+    }
+
+let private doNop(state:MachineState) arg : MachineState =
+   {state with
+       PC=state.PC+1;
+   }
+
+let rec private doOne state =
+    let line = split " " state.Program.[state.PC]
+    let cmd = line.[0]
+    let arg0 = int32 line.[1]
+
+    let middleState=
+        match cmd with
+            | "jmp" -> doJmp state arg0
+            | "acc" -> doAcc state arg0
+            | _ -> doNop state arg0
+
+    let newState = 
+        {middleState with 
+            VisitedLocations = middleState.VisitedLocations.Add middleState.PC; 
+        }
+    if newState.PC >= state.Program.Length then
+        {newState with RunState = ReachedEnd}
+    else
+        if state.VisitedLocations.Contains newState.PC then
+            {newState with RunState = LoopedBack}
+        else
+            newState |> doOne  
+
+let Run (state:MachineState) : MachineState =
+    doOne state
