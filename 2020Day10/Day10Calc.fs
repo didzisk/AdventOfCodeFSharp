e3fc781629e7f9e1ae8497ae4bc92d36b254f6c6
Didzis Klavins <didzis.klavins@visma.com>
2020-12-09 08:33:57 +0100

    Added Day 10 template

---
+++ 2020Day10/Day10Calc.fs
@@ -0,0 +1,10 @@
+﻿module Day10Calc
+
+open System.IO
+open Common
+open System.Text.RegularExpressions
+
+let Lines filename = 
+    File.ReadLines filename
+    |> Seq.toArray
+
---
+++ 2020Day10/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day10Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    Lines "Day10InputExample.txt"
+    |> printfn "%A"
+
+
+    0 // return an integer exit code
