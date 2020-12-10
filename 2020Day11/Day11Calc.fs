3db9433409fa6a478eb0cc0f626ceac3490dd790
Didzis Klavins <didzis.klavins@visma.com>
2020-12-10 15:18:13 +0100

    Day 11 template added

---
+++ 2020Day11/Day11Calc.fs
@@ -0,0 +1,9 @@
+﻿module Day11Calc
+
+open System.IO
+
+let Lines filename = 
+    File.ReadLines filename
+    |> Seq.map int
+    |> Seq.toArray
+
---
+++ 2020Day11/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day11Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    Lines "Day11InputExample.txt"
+    |> printfn "%A"
+
+
+
+
+
+
+
+    0 // return an integer exit code
