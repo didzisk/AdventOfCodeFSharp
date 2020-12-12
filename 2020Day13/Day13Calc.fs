29876d31160628f7aa26029370bff5ea1a075416
Didzis Klavins <didzis.klavins@visma.com>
2020-12-12 08:17:45 +0100

    Added Day 13 template

---
+++ 2020Day13/Day13Calc.fs
@@ -0,0 +1,8 @@
+﻿module Day13Calc
+
+open System
+open System.IO
+open Common
+
+let Lines filename = 
+    File.ReadLines filename
---
+++ 2020Day13/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day13Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    "Day13InputExample.txt"
+    |> Lines
+    |> Seq.iter (printfn "%s")
+
+
+
+
+
+
+
+
+
+    0 // return an integer exit code
