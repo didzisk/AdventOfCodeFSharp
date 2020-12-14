f7ca127f3c51591d9035821c3736d05f90df4508
Didzis Klavins <didzis.klavins@visma.com>
2020-12-14 23:05:10 +0100

    Day 15 template added

---
+++ 2020Day15/Day15Calc.fs
@@ -0,0 +1,10 @@
+﻿module Day15Calc
+
+open System
+open System.IO
+open Common
+
+let Lines filename = 
+    File.ReadLines filename
+    |> Seq.toArray
+ 
---
+++ 2020Day15/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day15Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    "Day15InputExample.txt"
+    |> Lines
+    |> Array.iter (printfn "%s")
+
+    0 // return an integer exit code
