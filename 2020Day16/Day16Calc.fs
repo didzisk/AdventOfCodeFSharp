83bfe09de09499da78001a62cfd69ed5692edd86
Didzis Klavins <didzis.klavins@visma.com>
2020-12-15 18:29:03 +0100

    Added Day 16 template

---
+++ 2020Day16/Day16Calc.fs
@@ -0,0 +1,9 @@
+﻿module Day16Calc
+
+open System
+open System.IO
+open Common
+
+let Lines filename =
+    File.ReadLines filename
+
---
+++ 2020Day16/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day16Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    "Day16InputExample.txt"
+    |> Lines
+    |> Seq.iter (printfn "%s")
+
+
+
+    0 // return an integer exit code
