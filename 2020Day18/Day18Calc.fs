5d8d0e8dfca1dc0de266a4ebac0d68b1895ded95
Didzis Klavins <didzis.klavins@visma.com>
2020-12-17 13:24:36 +0100

    Day 18 template added

---
+++ 2020Day18/Day18Calc.fs
@@ -0,0 +1,9 @@
+﻿module Day18Calc
+
+open System
+open System.IO
+open Common
+
+let Lines filename =
+    File.ReadLines filename
+    |> Seq.toArray
---
+++ 2020Day18/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day18Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    "Day18InputExample.txt"
+    |> Lines
+    |> Array.iter (printfn "%s")
+
+
+
+
+
+
+
+
+
+
+
+
+    0 // return an integer exit code
