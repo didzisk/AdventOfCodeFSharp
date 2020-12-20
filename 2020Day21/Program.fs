993a8ffd3831f85be470f96ea5b2b6bff050ae7e
Didzis Klavins <didzis.klavins@visma.com>
2020-12-20 22:22:56 +0100

    Day 21 template added

---
+++ 2020Day21/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day21Calc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    "Day21InputExample.txt"
+    |> Lines
+    |> printfn "%A"
+    0 // return an integer exit code
