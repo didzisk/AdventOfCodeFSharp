580361ff2248b8f6b0325000b311daecbdeab8c3
Didzis Klavins <didzis.klavins@gmail.com>
2021-12-01 18:04:08 +0100

    2021 Day02 template added

---
+++ 2021Day02/Day02Calc1.fs
@@ -0,0 +1,10 @@
+﻿module Day02Calc1
+
+open System
+open System.IO
+
+let Lines filename =
+    File.ReadLines filename
+    |> Seq.map int32
+    |> Seq.toArray
+
---
+++ 2021Day02/Program.fs
@@ -0,0 +1,10 @@
+﻿
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+open Day02Calc1
+
+"Day02Input.txt"
+|> Lines
+|> printfn "filtered %A"
