daabfd5425b27e6aa6108de6db54122f8447e22b
Didzis Klavins <didzis.klavins@gmail.com>
2021-12-01 17:55:54 +0100

    2021 Day1 completed

---
+++ 2021Day01/Day01Calc1.fs
@@ -0,0 +1,23 @@
+﻿module Day01Calc1
+
+open System
+open System.IO
+
+let Lines filename =
+    File.ReadLines filename
+    |> Seq.map int32
+    |> Seq.toArray
+
+let Increasing inpseq =
+    inpseq
+    |> Seq.pairwise
+    |> Seq.map(fun (x,y)->y-x)
+    |> Seq.filter(fun x-> x>0)
+    |> Seq.length
+
+let triplets inpseq =
+    seq {
+        let len = Array.length inpseq
+        for i = 0 to len-3 do
+            yield inpseq[i]+inpseq[i+1]+inpseq[i+2]
+    }
---
+++ 2021Day01/Program.fs
@@ -0,0 +1,43 @@
+﻿open Day01Calc1
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+"Day01InputExample.txt"
+|> Day01Calc1.Lines
+|> Seq.map int32
+|> Seq.pairwise
+|> printfn "%A"
+
+"Day01InputExample.txt"
+|> Day01Calc1.Lines
+|> Seq.map int32
+|> Seq.pairwise
+|> Seq.map(fun (x,y)->y-x)
+|> printfn "%A"
+
+"Day01InputExample.txt"
+|> Day01Calc1.Lines
+|> Seq.map int32
+|> Seq.pairwise
+|> Seq.map(fun (x,y)->y-x)
+|> Seq.filter(fun x-> x>0)
+|> printfn "filtered %A"
+
+
+"Day01Input.txt"
+|> Day01Calc1.Lines
+|> Increasing
+|> printfn "filtered %A"
+
+"Day01InputExample.txt"
+|> Day01Calc1.Lines
+|> triplets
+|> Increasing
+|> printfn "Part 2 ex %A"
+
+"Day01Input.txt"
+|> Day01Calc1.Lines
+|> triplets
+|> Increasing
+|> printfn "Part 2 ex %A"
