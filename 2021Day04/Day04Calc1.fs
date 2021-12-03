49b3face1510b164b3296defaf5911b956f71fcd
Didzis Klavins <didzis.klavins@gmail.com>
2021-12-03 22:45:19 +0100

    2021 Day 4 template

---
+++ 2021Day04/Day04Calc1.fs
@@ -0,0 +1,9 @@
+﻿module Day04Calc1
+
+open System
+open System.IO
+
+let Lines filename =
+    File.ReadLines filename
+    |> Seq.map int32
+    |> Seq.toArray
---
+++ 2021Day04/Program.fs
@@ -0,0 +1,8 @@
+﻿open Day04Calc1
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+"Day04InputExample.txt"
+|> Day04Calc1.Lines
+|> printfn "%A"
