8d36e70507f264207080d93e49d90bef8395b9c0
Didzis Klavins <didzis.klavins@gmail.com>
2021-12-03 20:27:56 +0100

    2021 Day 3 part 1 solved

---
+++ 2021Day03/Day03Calc1.fs
@@ -0,0 +1,124 @@
+﻿module Day03Calc1
+
+open System
+open System.IO
+
+let numZeros rows pos =
+    rows
+    
+
+type StatusReport = 
+    {
+        Zeros:int
+        Ones:int
+    }
+
+let initArr = [|{Zeros = 0;Ones = 0};{Zeros = 0;Ones = 0};{Zeros = 0;Ones = 0};
+    {Zeros = 0;Ones = 0};{Zeros = 0;Ones = 0};{Zeros = 0;Ones = 0};
+    {Zeros = 0;Ones = 0};{Zeros = 0;Ones = 0};{Zeros = 0;Ones = 0};
+    {Zeros = 0;Ones = 0};{Zeros = 0;Ones = 0};{Zeros = 0;Ones = 0};|]
+    
+    
+
+let folder1 numDigits (state:StatusReport[]) line =
+    seq{
+        for i = 0 to numDigits - 1 do
+            let mask = 1 <<< i
+            let newElm =
+                match line &&& mask with
+                | 0 -> { state[i] with Ones = 1 + state[i].Ones} 
+                | _ -> { state[i] with Zeros = 1 + state[i].Zeros} 
+            yield newElm
+    }
+    |> Seq.toArray
+        
+
+let Lines filename =
+    let all =
+        File.ReadLines filename
+    let numDigits = 
+        all
+        |> Seq.head
+        |> String.length
+    (
+    all
+        |> Seq.map(fun s-> "0b"+s)
+        |> Seq.map int32
+        , numDigits
+        )
+
+let Calc1 filename = 
+    let (all, numDigits) = Lines filename
+
+    let status = 
+        all
+        |> Seq.fold (folder1 numDigits) initArr
+    
+    let epsilon = 
+        seq{
+            for i = 0 to numDigits - 1 do
+                let mask = 1 <<< i
+                if status[i].Zeros > status[i].Ones then
+                    yield mask
+                else
+                    yield 0
+
+        }
+        |> Seq.sum
+    let gamma = 
+        seq{
+            for i = 0 to numDigits - 1 do
+                let mask = 1 <<< i
+                if status[i].Zeros < status[i].Ones then
+                    yield mask
+                else
+                    yield 0
+
+        }
+        |> Seq.sum
+
+    gamma * epsilon, $"gamma {gamma}" , $"epsilon {epsilon}"
+
+let folder2 numDigits (a:list<int32>) i =
+    printfn ""
+    a
+    |> List.iter (fun x-> printf $" b{Convert.ToString(x,2)} ")
+
+    if a.Length = 1 then
+        a
+    else
+        let statusReports = 
+            a
+            |> Seq.fold (folder1 (numDigits-i+1)) initArr
+        printfn "%A" statusReports
+
+        let statusReport = statusReports[0]
+
+        let mask = 1<<<(numDigits-i)
+        printfn
+        printfn $"mask b{Convert.ToString(mask,2)} "
+        if statusReport.Zeros < statusReport.Ones then
+            a
+            |> List.filter (
+                fun x-> 
+                    x &&& mask = 0
+                )
+        else
+            a
+            |> List.filter (
+                fun x-> 
+                    x &&& mask <> 0
+                )
+
+let Calc2 filename = 
+    let (all, numDigits) = Lines filename
+
+    let status = 
+        all
+        |> Seq.fold (folder1 numDigits) initArr
+
+    let oneFolder = folder2 numDigits
+
+    [1..numDigits]
+    |> List.fold oneFolder (Seq.toList(all))
+
---
+++ 2021Day03/Program.fs
@@ -0,0 +1,20 @@
+﻿open Day03Calc1
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+//"Day03Input.txt"
+//|> Calc1
+//|> printfn "1 res %A"
+
+"Day03InputExample.txt"
+|> Calc1
+|> printfn "1 ex %A"
+
+//"Day03Input.txt"
+//|> Calc2
+//|> printfn "2 res %A"
+
+"Day03InputExample.txt"
+|> Calc2
+|> printfn "2 ex %A"
