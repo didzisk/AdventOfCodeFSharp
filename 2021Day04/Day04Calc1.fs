4dbcbf05a275fbd16598e3f10c6171265ba29566
Didzis Klavins <didzis.klavins@gmail.com>
2021-12-11 13:46:34 +0100

    2021 Day 4 completed

--- 2021Day04/Day04Calc1.fs
+++ 2021Day04/Day04Calc1.fs
@@ -2,8 +2,162 @@
 
 open System
 open System.IO
+open Common
 
 let Lines filename =
     File.ReadLines filename
-    |> Seq.map int32
-    |> Seq.toArray
+
+
+let isEmptyRow arr =
+    arr
+    |> Array.forall (fun x->x="")
+
+let hasEmptyRow arr =
+    arr
+    |> Array.exists isEmptyRow
+
+let hasEmptyCol arr =
+    arr
+    |> Array.transpose
+    |> hasEmptyRow
+
+let arrSum (arr:string[][]) =
+    arr
+    |> Array.map (
+        fun x-> 
+            x
+            |> Array.map (fun x-> 
+                if x="" then
+                    0
+                else
+                    int32 x)
+            |> Array.sum
+        )
+    |> Array.sum
+
+let Calc1 filename =
+    let all = Lines filename
+    let commands = 
+        all
+        |> Seq.head
+        |> splitOn(",")
+
+
+    let arraysText = 
+        all 
+        |> Seq.skip 2
+        |> Seq.toArray
+
+    let numArrays = (arraysText.Length+1) / 6
+    let allArrays = 
+        [|
+            for i = 0 to numArrays-1 do
+                yield
+                    [| for row = 0 to 4 do
+                        yield arraysText[i*6+row]
+                            |> splitOn (" ")
+                    |]
+        |]
+
+    let nextState (allArrays:string[][][]) x =
+        [|
+            let numArrays = allArrays.Length
+            for i = 0 to numArrays-1 do
+                if (hasEmptyRow allArrays.[i]) || (hasEmptyCol allArrays.[i]) then
+                    ()
+                else
+                    yield
+                        [| for row = 0 to 4 do
+                            yield
+                                [|
+                                    for col = 0 to 4 do
+                                        if allArrays.[i].[row].[col] = x then
+                                            yield ""
+                                        else
+                                            yield allArrays.[i].[row].[col]
+                                |]
+                        |]
+        |]
+
+    let nextStateWithLog (allArrays:string[][][]) currCall =
+
+        printfn $"Current call: {currCall}"
+
+        let all = nextState allArrays currCall
+
+        all
+        |> Array.iter (fun x->
+            Array.iter (printfn "%A") x 
+            printfn ""
+            )
+
+        let oneArray = 
+            (
+            all
+            |> Array.filter(fun arr-> (hasEmptyRow arr) || (hasEmptyCol arr))
+            )
+
+        let cmd = 
+            if oneArray.Length>0 then
+                oneArray
+                |> Array.iter (printfn "%A")
+                printfn $"Solution {(arrSum oneArray.[0]) * (int32 currCall)} = {(arrSum oneArray.[0])} * {(int32 currCall)}"
+            else 
+                ()
+        cmd
+        
+        Console.ReadLine() |> ignore
+
+        all
+
+
+    allArrays
+    |> Array.iter (fun x->
+        Array.iter (printfn "%A") x 
+        printfn ""
+        )
+
+    commands
+    |> Seq.fold nextStateWithLog allArrays
+    |> ignore
+    
+
+    (*
+    commands
+    |> Seq.iter (fun x ->
+        for i = 0 to numArrays-1 do
+            for row = 0 to 4 do
+                for col = 0 to 4 do
+                    allArrays[i].[row].[col] <-
+                        if allArrays[i].[row].[col] = x then
+                            ""
+                        else
+                            allArrays[i].[row].[col]
+
+        //allArrays
+        //|> Array.iter (fun x->
+        //    Array.iter (printfn "%A") x 
+        //    printfn ""
+        //    )
+        let oneArray = 
+            (
+            allArrays
+            |> Array.filter(fun arr-> (hasEmptyRow arr) || (hasEmptyCol arr))
+            )
+
+        let cmd = 
+            if oneArray.Length>0 then
+                oneArray
+                |> Array.iter (printfn "%A")
+                printfn $"Solution {(arrSum oneArray.[0]) * (int32 x)}"
+                failwith "done"
+            else 
+                ()
+        cmd
+
+        //printfn "##########################################"
+
+        //Console.ReadLine() |> ignore
+        )
+    *)
+
--- 2021Day04/Program.fs
+++ 2021Day04/Program.fs
@@ -3,6 +3,12 @@
 // For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
 
-"Day04InputExample.txt"
-|> Day04Calc1.Lines
-|> printfn "%A"
+
+//"Day04InputExample.txt"
+//|> Day04Calc1.Calc1
+//|> ignore
+
+
+"Day04Input.txt"
+|> Day04Calc1.Calc1
+|> ignore
