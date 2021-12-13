245cb08bbab105b9e317586d45c2da7f8043e427
Didzis Klavins <didzis.klavins@gmail.com>
2021-12-13 15:19:38 +0100

    2021 Day 13 solved

---
+++ 2021Day13/Day13Calc.fs
@@ -0,0 +1,90 @@
+﻿module Day13Calc
+
+open System.IO
+open System
+open Common
+
+[<Struct>]
+type Point = 
+    { X: int
+      Y: int}
+
+type FoldCommand =
+    | FoldX of int
+    | FoldY of int
+
+let parselines filename =
+    let arr =
+        File.ReadAllLines filename
+        |> Seq.toArray
+    let theEmpty =
+        arr
+        |> Array.findIndex (fun line -> line = "")
+
+    let paper =
+        arr
+        |> Array.take theEmpty
+        |> Array.map (fun line->
+            let coords = split "," line
+            let x = int coords[0]
+            let y = int coords[1]
+            {X=x;Y=y}
+            )
+        |> Set.ofArray
+
+    let folds =
+        arr
+        |> Array.skip (theEmpty+1)
+        |> Array.map (fun line->
+            let cmd = split "=" line
+            if cmd[0] = "fold along x" then
+                FoldX (int cmd[1])
+            else
+                FoldY (int cmd[1])
+            )
+
+    paper, folds
+
+let folder (paper:Set<Point>) cmd = 
+    paper
+    |> Set.map (fun pt ->
+        match cmd with
+        | FoldX foldx -> 
+            {pt with 
+                X =
+                    if foldx > pt.X then 
+                        pt.X
+                    else
+                        2 * foldx-pt.X
+            }
+        | FoldY foldy -> 
+            {pt with
+                Y =
+                    if foldy > pt.Y then
+                        pt.Y
+                    else
+                        2 * foldy - pt.Y
+            }
+        )
+
+let Calc1 filename =
+    let paper, folds =
+        parselines filename
+
+    folder paper folds[0] 
+    |> Set.count
+
+let showpaper (paper:Set<Point>) = 
+    Console.Clear()
+    paper
+    |> Set.iter(fun pt->
+        Console.CursorTop<-pt.Y
+        Console.CursorLeft<-pt.X
+        Console.Write("#")
+        )
+    Console.ReadLine() |> ignore
+        
+let Fold2 filename =
+    let paper, folds = parselines filename
+
+    Seq.fold folder paper folds
---
+++ 2021Day13/Program.fs
@@ -0,0 +1,19 @@
+﻿open Day13Calc
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+parselines "Day13InputExample.txt"
+|> printfn "%A"
+
+
+Calc1 "Day13Input.txt"
+|> printfn "%A"
+
+Fold2 "Day13InputExample.txt"
+|> showpaper
+
+
+Fold2 "Day13Input.txt"
+|> showpaper
+
